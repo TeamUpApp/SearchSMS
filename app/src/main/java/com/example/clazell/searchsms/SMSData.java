4c01ec4dda03ff38499975b91c14e2f9b587208b
Cameron Lazell <cameron.lazell@lisasoft.com>
2014-10-22 14:11:59 +1100

    Add custon array adapter and contact names

---
+++ app/src/main/java/com/example/clazell/searchsms/SMSData.java
@@ -0,0 +1,16 @@
+package com.example.clazell.searchsms;
+
+/**
+ * Created by clazell on 22/10/2014.
+ */
+public class SMSData {
+
+        public String name;
+        public String Body;
+
+        public SMSData(String name, String Body) {
+            this.name = name;
+            this.Body = Body;
+        }
+
+}
